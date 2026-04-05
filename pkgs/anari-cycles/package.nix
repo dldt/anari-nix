62ed188702ec1be5bf92a7b53b8d31e95144e7a8
Thomas Arcila <134677+tarcila@users.noreply.github.com>
2026-04-05 18:23:36 -0400

    Fix anari-cycles

--- pkgs/anari-cycles/package.nix
+++ pkgs/anari-cycles/package.nix
@@ -15,6 +15,8 @@
   libtiff,
   libGL,
   python3,
+  opencolorio,
+  openimagedenoise,
   openimageio,
   openvdb,
   openexr,
@@ -47,6 +49,7 @@ stdenv.mkDerivation {
     ./0003-Link-with-IOKit-on-when-building-Metal.patch
     ./0004-Do-not-build-cycles-standalone-app.patch
     ./0005-Revert-Build-Use-CMAKE_CURRENT_SOURCE_DIR-for-findin.patch
+    ./0006-Fix-missing-include-in-OSL-services.patch
   ];
 
   nativeBuildInputs = [
@@ -63,6 +66,8 @@ stdenv.mkDerivation {
     libpng
     libtiff
     openexr
+    opencolorio
+    openimagedenoise
     openimageio
     openjpeg
     openvdb
